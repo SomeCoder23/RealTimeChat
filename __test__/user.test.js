61304f28746a6940b9eb52fe20689cce3eec9225
raghadtariq <raghaddrabee@gmail.com>
2023-10-23 02:19:04 +0300

    unit testing

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -4,7 +4,7 @@ import express from "express";
 import request from "supertest";
 import usersRouter from "../dist/routes/users.js";
 import dataSource from "../dist/db/dataSource.js";
-import { login } from "../dist/controllers/user.js";
+import {createUser, login } from "../dist/controllers/user.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -20,7 +20,7 @@ beforeAll(async () => {
     }).catch(err => {
         console.log("DB connection failed", err);
     });
-}, 30000);
+}, 100000);
 
 // afterAll(async () => {
 //   await dataSource.destroy();
@@ -39,14 +39,44 @@ describe("Login process", () => {
   });
 });
 
-describe("Logout process", () => {
-  it("should logout with valid credentials", async () => {
+// describe("Logout process", () => {
+//   it("should logout with valid credentials", async () => {
+//     const user = {
+//       username: "raghad"
+//     };
+
+//     const response = await  request(app).post("/users/logout").send(user);
+
+//     expect(response.status).toBe(200);
+//   });
+// });
+
+describe("register process", () => {
+  it("should register with valid credentials", async () => {
     const user = {
-      username: "raghad"
+       fullName : "roaa shaheen",
+       username:"rorooo",
+       password:"123456",
+       birthday: "2003-08-10"
     };
 
-    const response = await  request(app).post("/users/login").send(user);
+    const response = await  request(app).post("/users/register").send(user);
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(201);
+  });
+});
+
+describe("register process", () => {
+  it("should register with valid credentials", async () => {
+    const user = {
+       fullName : "roaa shaheen",
+       username:"rorooo",
+       password:"123456",
+       birthday: "2003-08-10"
+    };
+
+    const response = await  request(app).post("/chat/create_group").send(user);
+
+    expect(response.status).toBe(201);
   });
 });
